9c417028be509d99b69993f3f21a10c04ff619a4
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-01-31 23:10:43 +0200

    add response test's

--- src/services/whatsapp-web.js
+++ src/services/whatsapp-web.js
@@ -1,10 +1,15 @@
+const dotenv = require ("dotenv");
+
 const qrcode = require("qrcode-terminal");
 
 const { Client } = require("whatsapp-web.js");
 
 const state = require("./state.js");
 
+dotenv.config();
+
 const client = new Client();
+let count = 0;
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
@@ -19,6 +24,25 @@ client.on("message", async (msg) => {
   // console.log(msg.from)
 
   state.saveJson(msg);
+
+  
+  const contact = process.env.contact
+
+
+  if(msg.from === contact){
+    switch(count){
+      case 0:
+      case 3:
+      case 9:
+        msg.reply("Hello My love! 😍");break;
+      case 1:msg.reply("❤️❤️");break;
+      case 2: msg.reply("😍😍😍");break;
+      case 4: msg.reply("I love you 👸");break;
+      case 5: msg.reply("I love you more😍");break;
+      default: msg.reply("Honey 😘");break;
+    }
+    count ++;
+  }
 
   if (msg.body === "!claudia") {
     msg.reply("Hello My love! 😍");
